a6e480119d165b526eb04b8efd0855dac34d3923
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-30 13:02:21 -0700

    Moves BarberShop.cpp functions into BarberShop to match instructions

--- Assignment3/src/BarberShop.h
+++ Assignment3/src/BarberShop.h
@@ -10,16 +10,48 @@
 
 #include "customer.h"
 #include "LinkedStack.h"
+using namespace std;
 
 class BarberShop
 {
 	private:
-		LinkedStack<Customer> queue;
+		LinkedStack<Customer> s1;
+		LinkedStack<Customer> s2;
 	public:
 		BarberShop();
-		void addCustomer(Customer customer);
+		void addCustomer(Customer& customer);
 		Customer nextCustomer();
 		virtual ~BarberShop();
 };
+
+BarberShop::BarberShop()
+{
+	this->s1 = LinkedStack<Customer>();
+	this->s2 = LinkedStack<Customer>();
+}
+
+inline void BarberShop::addCustomer(Customer& customer)
+{
+	while(!s1.isEmpty())
+	{
+		s2.push(s1.pop());
+	}
+	s2.push(customer);
+
+	while(!s2.isEmpty())
+	{
+		s1.push(s2.pop());
+	}
+}
+
+inline Customer BarberShop::nextCustomer()
+{
+	return this->s1.pop();
+}
+
+inline BarberShop::~BarberShop()
+{
+	// TODO Auto-generated destructor stub
+}
 
 #endif /* BARBERSHOP_H_ */
